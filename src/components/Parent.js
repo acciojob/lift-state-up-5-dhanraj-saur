1b26b932c296e8b9b183e53f690f5c4d2c6c392b
dhanraj-saur <dhananjayverma301@gmail.com>
2023-07-13 12:05:14 +0530

    done

---
+++ src/components/Parent.js
@@ -0,0 +1,20 @@
+
+import  React, { useState } from 'react'
+import Child from './Child'
+const Parent = () =>{
+
+
+    let [isLoggedIn,setIsLoggedIn] = useState(false);
+
+    
+    return(
+        <div className='parent'>
+            <h1>Parent Component</h1>
+            <Child 
+                isLoggedIn={isLoggedIn}
+                setIsLoggedIn={setIsLoggedIn}
+            />
+        </div>
+    )
+}
+export default Parent
